81ff72a570d2505ebb3f973ed0e3b63396327786
kadownie <kadownie@gmail.com>
2017-10-19 08:55:02 -0400

    added method for solving for x

--- src/my/calculator/SimpleCalculator.java
+++ src/my/calculator/SimpleCalculator.java
@@ -9,7 +9,7 @@ public class SimpleCalculator {
 	    
 	private enum Operator
     {
-        ADD(1), SUBTRACT(2), MULTIPLY(3), DIVIDE(4), TRIG(5);
+        ADD(1), SUBTRACT(2), MULTIPLY(3), DIVIDE(4), EXP(5), TRIG(6);
         final int precedence;
         Operator(int p) { precedence = p; }
     }
@@ -19,6 +19,7 @@ public class SimpleCalculator {
         put("-", Operator.SUBTRACT);
         put("*", Operator.MULTIPLY);
         put("/", Operator.DIVIDE);
+        put("^", Operator.EXP);
         put("sin", Operator.TRIG);
         put("cos", Operator.TRIG);
         put("tan", Operator.TRIG);
@@ -50,38 +51,53 @@ public class SimpleCalculator {
         List <String> nums = new ArrayList<>();
         int count = 0;
         for(int i = 0; i < infix.length(); i++) {
-        	if(Character.isDigit(infix.charAt(i)) && i == infix.length()-1) {
-        		nums.add(infix.substring(i-count, i+1));
-        	}
-        	else if(Character.isLetter(infix.charAt(i))) {
-        		int k = 0;
-        		for(int j = i; j < infix.length(); j++) {
-        			if(!Character.isLetter(infix.charAt(j))) {
-        				break;
-        			}
-        			k++;
+        		if(Character.isDigit(infix.charAt(i)) && i == infix.length()-1) {
+        			nums.add(infix.substring(i-count, i+1));
         		}
-        		nums.add(infix.substring(i, k));
-        		i += k-1;
-        	}
-        	else if(Character.isDigit(infix.charAt(i))) {
-        		count++;
-        	}
-        	else {
-        		nums.add(infix.substring(i-count, i) + " ");
-        		nums.add(infix.substring(i, i+1) + " ");
-        		count = 0;
-        	}
+        		else if(i < infix.length()-1 && Character.isDigit(infix.charAt(i)) && Character.isLetter(infix.charAt(i+1))) {
+        			nums.add(infix.substring(i-count, i+1) + " * ");
+        			int k = 0;
+	        		for(int j = i+1; j < infix.length(); j++) {
+	        			if(!Character.isLetter(infix.charAt(j))) {
+	        				break;
+	        			}
+	        			k++;
+	        		}
+	        		nums.add(infix.substring(i+1, k+i+1));
+	        		i += k;
+        			count = 0;
+        		}
+        		else if(Character.isLetter(infix.charAt(i))) {
+	        		int k = 0;
+	        		for(int j = i; j < infix.length(); j++) {
+	        			if(!Character.isLetter(infix.charAt(j))) {
+	        				break;
+	        			}
+	        			k++;
+	        		}
+	        		nums.add(infix.substring(i, i+k));
+	        		i += k-1;
+        		}
+        		
+        		else if(Character.isDigit(infix.charAt(i))) {
+        			count++;
+        		}
+        		else {
+        			nums.add(infix.substring(i-count, i) + " ");
+        			nums.add(infix.substring(i, i+1) + " ");
+        			count = 0;
+        		}
         }
         for(int i = 0; i < nums.size(); i++) {
-        	if(nums.get(i).equals(" ")) {
-        		nums.remove(i);
-        	}
+        		if(nums.get(i).equals(" ")) {
+        			nums.remove(i);
+        		}
         }
         String infix2 = "";
         for(int i = 0; i < nums.size(); i++) {
-        	infix2 += nums.get(i) + " ";
+        		infix2 += nums.get(i) + " ";
         }
+        System.out.println(infix2);
 
         for (String token : infix2.trim().split("\\s")) {
             
@@ -212,12 +228,71 @@ public class SimpleCalculator {
 		
 		return stack.pop();
 		}
+	
+	public static BigDecimal generateRandom(BigDecimal min, BigDecimal max) {
+	    BigDecimal randomBigDecimal = min.add(new BigDecimal(Math.random()).multiply(max.subtract(min)));
+	    return randomBigDecimal.setScale(4,BigDecimal.ROUND_HALF_EVEN);
+	}
+	
+	BigDecimal algebra(String str) {
+		BigDecimal x1 = new BigDecimal("-10000000000000000000");
+		BigDecimal x2 = new BigDecimal("10000000000000000000");
+		BigDecimal x3 = BigDecimal.ZERO;
+		String prn1 = str.replaceAll("x", x1.toPlainString());
+		String prn2 = str.replaceAll("x", x2.toPlainString());
+		String prn3 = str.replaceAll("x", x3.toPlainString());
+		double dubfx1 = compute(prn1).doubleValue();
+		double dubfx2 = compute(prn2).doubleValue();
+		if(dubfx1 > 0) {
+			x1 = new BigDecimal("10000000000000000000");
+			x2 = new BigDecimal("-10000000000000000000");
+		}
+//		while(dubfx1 < 0) {
+//			x1 = generateRandom(new BigDecimal(min).setScale(4, BigDecimal.ROUND_HALF_EVEN), new BigDecimal(max).setScale(4, BigDecimal.ROUND_HALF_EVEN));
+//			prn1 = str.replaceAll("x", x1.toPlainString());
+//			dubfx1 = compute(prn1).doubleValue();
+//		}
+//		while(dubfx2 > 0) {
+//			x2 = generateRandom(new BigDecimal(min).setScale(4, BigDecimal.ROUND_HALF_EVEN), new BigDecimal(max).setScale(4, BigDecimal.ROUND_HALF_EVEN));
+//			prn2 = str.replaceAll("x", x2.toPlainString());
+//			dubfx2 = compute(prn1).doubleValue();
+//		}
+		BigDecimal fx1 = new BigDecimal(dubfx1);
+		BigDecimal fx2 = new BigDecimal(dubfx2);
+		BigDecimal fx3 = BigDecimal.ZERO;
+		while(Math.abs(x1.doubleValue()-x2.doubleValue()) > .000001) {
+			x3 = x1.add(x2).divide(new BigDecimal("2")).setScale(10, BigDecimal.ROUND_HALF_EVEN);
+			prn3 = str.replaceAll("x", x3.toPlainString());
+			fx3 = compute(prn3);
+			if(fx3.floatValue() >= 0) {
+				x2 = x3;
+			}
+			if(fx3.floatValue() <= 0) {
+				x1 = x3;
+			}
+			System.out.println(x1.toPlainString());
+			System.out.println(x2.toPlainString());
+			System.out.println(Math.abs(x1.doubleValue()-x2.doubleValue()));
+		}
+		return x1;
+	}
 	    
+	
+	
 	    
     BigDecimal solve(String str) {
-    	
-    	String prn = postfix(str);
-    	BigDecimal result = compute(prn).setScale(4, BigDecimal.ROUND_HALF_EVEN);
-    	return result;
+    		if(str.contains("=")) {
+    			int equalIndex = str.indexOf("=");
+    			String movedOver = str.substring(0, equalIndex) + " - ( " + str.substring(equalIndex+1, str.length()) + " )";
+    			String prn = postfix(movedOver);
+    			System.out.println(prn);
+    			BigDecimal result = algebra(prn).setScale(6, BigDecimal.ROUND_HALF_EVEN);
+    			return result;
+    		}
+    		else {
+		    	String prn = postfix(str);
+		    	BigDecimal result = compute(prn).setScale(4, BigDecimal.ROUND_HALF_EVEN);
+		    	return result;
+    		}
     }
 }
